56d030d9c3856efc4312b70a2a11da2d6841e89e
Suliman Alsarraf <suliman2@pdx.edu>
2020-03-20 15:27:20 -0700

    Only load container when custom font is loaded

--- App.js
+++ App.js
@@ -3,16 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 
 export default class main extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
   async componentDidMount() {
     await Font.loadAsync({
       'ACR': require('./assets/fonts/AsapCondensed-Regular.ttf'),
     });
+    this.setState({fontLoaded: true});
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.titleText}>PUBG Reports</Text>
-      </View>
+      this.state.fontLoaded ? (
+        <View style={styles.container}>
+          <Text style={styles.titleText}>PUBG Reports</Text>
+        </View>
+      ) : null
     );
   }
 }
