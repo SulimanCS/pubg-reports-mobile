fac975fa1c4d12f95a75656259c173dcd42d43b4
Suliman Alsarraf <suliman2@pdx.edu>
2020-03-20 14:56:36 -0700

    Change functional component to class component, change text

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class main extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titleText}>PUBG Reports</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
