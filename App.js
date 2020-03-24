e3d24306899c208605c3ea027a712cc039f1ae45
Suliman Alsarraf <suliman2@pdx.edu>
2020-03-23 23:08:44 -0700

    Create and use stack navigator

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './components/HomeScreen/HomeScreen';
+
+const Stack = createStackNavigator();
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <View>
-        <HomeScreen />
-      </View>
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
     );
   }
 }
