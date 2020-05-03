fab7b438fcb80755ef172e1a2d65fda61e1a3805
Suliman Alsarraf <suliman2@pdx.edu>
2020-05-02 17:17:13 -0700

    Load friends from local storage

--- components/Friends/FriendsList.js
+++ components/Friends/FriendsList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-paper";
+import * as SecureStore from "expo-secure-store";
 import styles from "./FriendsListStyles";
 
 export default class Friends extends React.Component {
@@ -10,12 +11,40 @@ export default class Friends extends React.Component {
   };
 
   async componentDidMount() {
+    await this.loadFriends();
     this.setState({
       friendsLoaded: true,
     });
   }
 
+  loadFriends = async () => {
+    const lastAddedFriendIDKey = "lastAddedFriendID";
+    const lastAddedFriendID = parseInt(
+      await SecureStore.getItemAsync(lastAddedFriendIDKey)
     );
+    console.log(lastAddedFriendID);
+    if (lastAddedFriendID == null) return;
+    console.log("for oo");
+    for (let i = 1; i < lastAddedFriendID + 1; i++) {
+      console.log(i);
+      const friendGameIDKey = "Friend" + i + "gameID";
+      const friendAccountIDKey = "Friend" + i + "accountID";
+      const friendPlatformKey = "Friend" + i + "platform";
+
+      const gameID = await SecureStore.getItemAsync(friendGameIDKey);
+      const accountID = await SecureStore.getItemAsync(friendAccountIDKey);
+      const platform = await SecureStore.getItemAsync(friendPlatformKey);
+
+      const frinedObject = {
+        gameID: gameID,
+        accountID: accountID,
+        platform: platform,
+      };
+      this.state.friends.push(frinedObject);
+    }
+    console.log(this.state.friends);
+  };
+
   render() {
     return this.state.friendsLoaded ? (
       this.state.friends.length == 0 ? (
