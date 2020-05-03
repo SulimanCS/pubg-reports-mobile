8df318366b7ff2e89ddbb49bfc3f1f8a193d195f
Suliman Alsarraf <suliman2@pdx.edu>
2020-05-02 17:06:51 -0700

    Render FriendsList screen based on the length of Friends in local storage (empty vs non-empty)

--- components/Friends/FriendsList.js
+++ components/Friends/FriendsList.js
@@ -4,12 +4,31 @@ import { Button } from "react-native-paper";
 import styles from "./FriendsListStyles";
 
 export default class Friends extends React.Component {
-  componentDidMount() {}
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.titleText}>Friends List</Text>
-      </View>
+  state = {
+    friends: [],
+    friendsLoaded: false,
+  };
+
+  async componentDidMount() {
+    this.setState({
+      friendsLoaded: true,
+    });
+  }
+
     );
+  render() {
+    return this.state.friendsLoaded ? (
+      this.state.friends.length == 0 ? (
+        <View style={[styles.container, { justifyContent: "center" }]}>
+          <Text style={[styles.titleText, { marginTop: 0 }]}>
+            Your Friends List Is Empty
+          </Text>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <Text style={styles.titleText}>Friends List</Text>
+        </View>
+      )
+    ) : null;
   }
 }
