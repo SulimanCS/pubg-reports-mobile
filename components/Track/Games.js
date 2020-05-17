2e764183eb6164639bcdeb6c19a82930fb1d9081
Suliman Alsarraf <suliman2@pdx.edu>
2020-05-17 08:22:42 -0700

    Create a surface generator for game to generate react native paper surfaces

--- components/Track/Games.js
+++ components/Track/Games.js
@@ -17,6 +17,42 @@ export default class Games extends React.Component {
       games: games,
     });
   }
+  generateGameSuface = (options) => {
+    const lostBG = { backgroundColor: "#A0060F" };
+    const lostColor = { color: "#fff" };
+    const wonBG = { backgroundColor: "#0E7A0D" };
+    const wonColor = { color: "#F0E68C" };
+
+    const background = options.rank === 1 ? wonBG : lostBG;
+    const title = options.rank === 1 ? "WON" : "LOST";
+    const color = options.rank === 1 ? wonColor : lostColor;
+
+    return (
+      <View>
+        <Surface style={[styles.surfaceLong, background]}>
+          <View
+            style={[styles.textCentered, { justifyContent: "space-between" }]}
+          >
+            <Text
+              style={[styles.surfaceText, color]}
+            >{`#${options.rank}`}</Text>
+            <View
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+              }}
+            >
+              <Text style={[styles.surfaceText, color]}>{title}</Text>
+            </View>
+          </View>
+        </Surface>
+      </View>
+    );
+  };
+
 
   render() {
     const { navigation } = this.props;
