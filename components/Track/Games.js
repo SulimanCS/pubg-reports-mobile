7c9297e27cb270288a8db8a0400d2316cd0fe78e
Suliman Alsarraf <suliman2@pdx.edu>
2020-05-17 08:06:23 -0700

    Set state for ID, platform and game objects when Game component mounts

--- components/Track/Games.js
+++ components/Track/Games.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Surface } from "react-native-paper";
 import styles from "./TrackStyles";
 
 export default class Games extends React.Component {
-  render() {
-    const { navigation } = this.props;
+  state = {
+    games: [],
+  };
+  componentDidMount() {
+    const { games } = this.props.route.params;
     const { ID } = this.props.route.params;
     const { platform } = this.props.route.params;
+    this.setState({
+      ID: ID,
+      platform: platform,
+      games: games,
+    });
+  }
+
+  render() {
+    const { navigation } = this.props;
     return (
       <View style={styles.container}>
         <Text style={styles.titleText}>
-          Tracking: {" " + ID + " " + "(" + platform + ")"}
+          Tracking:{" "}
+          {" " + this.state.ID + " " + "(" + this.state.platform + ")"}
         </Text>
       </View>
     );
