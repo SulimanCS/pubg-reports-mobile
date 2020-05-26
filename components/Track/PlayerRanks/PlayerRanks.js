dcb0cccad7188746beb420a07b06d8a6076fb2ec
Suliman Alsarraf <suliman2@pdx.edu>
2020-05-25 19:18:48 -0700

    Create a surface generator for players to generate react native paper surfaces for PlayerRanks

--- components/Track/PlayerRanks/PlayerRanks.js
+++ components/Track/PlayerRanks/PlayerRanks.js
@@ -13,6 +13,36 @@ export default class PlayerRanks extends React.Component {
     const { playerStats } = this.props.route.params;
     this.setState({playerStats: playerStats})
   }
+  generatePlayerSurface = (options) => {
+    return (
+      <Surface style={styles.surfaceLong}>
+        <TouchableOpacity>
+          <View
+            style={[styles.textCentered, { justifyContent: "space-between" }]}
+          >
+            <Text style={styles.surfaceText}>{`#${options.rank}`}</Text>
+            <View
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+              }}
+            >
+              <Text style={styles.surfaceText}>{options.playerName}</Text>
+            </View>
+            <View>
+              <Text style={[styles.surfaceText, { color: "#A0060F" }]}>
+                {options.kills}
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </Surface>
+    );
+  };
+
 
   render() {
     return (
